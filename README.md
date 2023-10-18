0ad3dedce99570e36416624fd4de19de11eedd91
Matthias Moik <matthias.moik@hotmail.com>
2023-10-18 17:12:11 +0200

    move assignment 1 to separate file, clean up current code

--- README.md
+++ README.md
@@ -1,64 +1,12 @@
-# ADL_W23: Dataset generation for Single German playing cards and object detection using YOLO
+# Project_name
 
-## Introduction
+In this project for the course 'Applied deep learning' at the TU Wien, we aim to build an application, that is capable of keeping score of players in the game 'Jass'. For this we build our own dataset, because the cards used for this game are not standard poker cards and we found no dataset online. A YOLO model will be trained on this dataset afterwards. Implementing the model in an application that can detect cards in realtime and is easy to use wraps up the project.
 
-Playing card detection is a specific topic in the area of computer vision and there are already working models out there, that can correctly identify playing cards in real time. This can be used to track a game for spectators, like it is mentioned in this paper about dataset generation for the game [Duplicate bridge](https://arxiv.org/pdf/2109.11861.pdf), to help players train [card counting](https://www.youtube.com/watch?v=Nf3zBJ2cDAs), or to keep track of the scores for each player. The latter application is also interesting for a game played in Vorarlberg, Austria called "*Jass*". In the end of each round, every player has to calculate their score by adding up the different values of the cards they won during the round. Unlike the cards used in the examples above, Jassa uses *Single German playing cards*, that are not as commonly used in other places. Because of this we were not able to find any dataset containing this type of cards, which will be adressed in this project.
+## Submissions for the Course
 
-## Project
-
-The main part of this project will be to generate a suitable dataset for neural network training for Single German playing cards with a similar method proposed in the paper (reference). This dataset will then be used to train YOLO (reference), that is able to detect multiple objects in an image very fast. In the end we should be able to place multiple playing cards under a camera and let the program detect the position using bounding boxes and also the suit and value of the card.
-
-Wether detection in different rotations of a card and partially overlapping cards is possible, will be interesting to see. This is because unlike poker cards, Single German playing cards are not symmetrical and even more important, they do not have their suit and value written in the corners. This makes classification of overlapping cards much more difficult and is therefore not a main goal of the project.
-
-To wrap up the whole project, this playing card detection model will be integrated into an application, that is able to track a whole game and calculate the scores for each player.
+The submission of the first assignment can be found in [A1_Initiate.md](./assignments/A1_Initiate.md).
 
 
-### Dataset
+## Usage of Application
 
-To create a dataset, use a similar method as in [this paper](https://arxiv.org/pdf/2109.11861.pdf) We take pictures of the cards under different light conditions, annotate them once by hand and then generate images automatically. Features that will be considered in these images will be the following:
-
-- Background
-- Number of cards
-- Position
-- Light settings
-- Size
-- Rotation
-- Angle
-
-If all of these make sense to use and lead to an improvement will be seen during the evaluation of the trained models.
-
-Indentical to the paper, we use the 'Describing Textures Dataset' [DTD](https://www.robots.ox.ac.uk/~vgg/data/dtd/) as backgrounds for our generated images.
-
-The dataset generation will be implemented in a way that makes it easy to generate new datasets of a similar type (e.g. another type of playing cards). The way we intend it to work in the end is the following:
-
-- Shoot short videos of each playing card with different light settings, with the camera straight above the card
-- Giving the path to the folder containing the videos will suffice to process the videos, ending in cropped images of each card with differing lightning
-- Giving the path to the folder containing the cropped images and parameters like number of images and the properties of the cards on the images (see above), generates automatically a dataset in the right format for YOLO to use.
-
-### Model
-
-The model we will be training is called YOLO (reference), that is currently one of the best models in detecting multiple objects in a single image in a very short amount of time. This makes it fitting for our task to detect multiple cards at once.
-
-
-
-
-
-# Work-breakdown and schedule
-
-Research for assignment 1
-
-|Topic|Short description|Anticipated time in hours|
-|---|---|---|
-|Project idea|Come up with interesting ideas and select one of them|1|
-|Research|Find Papers, come up with promising strategy, |5|
-|Take photos|Create suitable and reproducable setup, documentat steps, shoot videos|10|
-|Implement dataset generation|Strategy, find useful packages, implementing, testing, variable parameters, aim for minimal user input|20|
-|Model Training|Getting familiar with model, create setup for training and evaluation, train model, fine-tune with model parameters and dataset-generation parameters|20|
-|Test with live data|Test model with live data from a connected camera|5|
-|Application|Implement model into application, detect cards, keep score of players|14|
-
-# References
-
-The currently used papers and websites for reference and inspiration are partially linked in this README.md file, but can also be found gathered together in this [file](./references/links.md).
-
-
+todo
--- src/dataset_generation_functions.py
+++ src/dataset_generation_functions.py
@@ -3,148 +3,206 @@ import cv2 as cv
 from pathlib import Path
 import os
 import random as rand
+import imutils
+import photo_preparation_functions as ppf
 
 
-def select_background(backgrounds_path):
-    background_paths = [str(path) for path in Path(backgrounds_path).glob('**/*.jpg')]
-    selected_background_path = rand.sample(background_paths, 1)[0]
-    background = cv.imread(selected_background_path)
-    # cv.imshow('background', background)
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-    return background
+def find_edges(img):
+    blurred_img = cv.GaussianBlur(img.copy(),(5,5),0)
+    edged_img = cv.Canny(blurred_img,50,100)
+    return edged_img
 
-
-def select_cards(cards_path, number_of_cards):
-    card_paths = [str(path) for path in Path(cards_path).glob('*')]
-    selected_cards_paths = rand.sample(card_paths, number_of_cards)
-    selected_cards_names = [card_path.split('/')[-1].split('_')[0] for card_path in selected_cards_paths]
-
-    cards = []
-    for path in selected_cards_paths:
-        card = cv.imread(path)
-        cards.append(card)
-
-    # cv.imshow('card 1', cards[0])
-    # cv.waitKey(0)
-    # cv.destroyAllWindows()
-    return cards, selected_cards_names
-
-
-def generate_yaml_file(PLAYING_CARDS_DIR, GENERATED_DATASET_DIR):
-    video_names = [str(path).split('/')[-1].split('.')[0] for path in Path(PLAYING_CARDS_DIR).glob('*')]
-    video_list_unique = list(set(video_names))
-    video_list_unique.sort()
-
-    classes_dict = dict((name, i) for i, name in enumerate(video_list_unique))
-    number_of_classes = len(video_list_unique)
-
-    with open(GENERATED_DATASET_DIR + '/classes.yaml', 'w') as file:
-        file.write('classes:\n')
-        for name in video_list_unique:
-            file.write('- ' + name + '\n')
-        file.write(f'nc: {number_of_classes}')
-    
-    return classes_dict
-
-def place_card(background, card, size, rotation, angle):
-    #todo
-    boundingbox = 0
-    image = 0
-    return image, boundingbox
-
-
-def bb_overlap(bounding_boxes, bounding_box):
-    #todo
-    pass
-
-
-def add_new_bb(bounding_boxes, bounding_box):
-    #todo
-    pass
+def create_mask(img, edged_img):
+    contours,_ = cv.findContours(edged_img.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+    filled_card = cv.fillPoly(img.copy(), [max(contours, key = cv.contourArea)], color = (255, 255, 255))
+    _, mask = cv.threshold(filled_card, thresh= 180, maxval = 255, type = cv.THRESH_BINARY)
+    return mask
 
 
 
 
-def place_cards(background, cards, classes, max_size, min_size, max_rotation = 0, max_angle = 0, overlapping = False):
-    background_hight, background_width, _ = background.shape
-    card_hight, card_width, _ = cards[0].shape
-    image = background
-
-    bounding_boxes = list()
-
-    while i in range(len(cards)):
-        size = rand.uniform(min_size, max_size)
-        rotation = rand.uniform(0, max_rotation)
-        angle = rand.uniform(max_angle)
-
-        image_proposal, bounding_box_proposal = place_card(image, size, rotation, angle)
-        
-        if (bb_overlap(bounding_boxes, bounding_box_proposal)) and (not overlapping):
-            i -= 1
-        else:
-            image = image_proposal
-            bounding_boxes = add_new_bb(bounding_boxes, bounding_box_proposal)
-
-    labels = ''
-    for i, bounding_box in enumerate(bounding_boxes):
-        labels += classes[i] + ' ' + ' '.join(bounding_box) + '\n'
-    return image, labels
 
 
-def generate_dataset(BACKGROUNDS_DIR, PLAYING_CARDS_DIR, PROCESSED_PLAYING_CARDS_DIR, GENERATED_DATASET_DIR, number_of_images, max_number_of_cards, min_size, max_size, max_rotation, max_angle, overlapping, seed):
-    rand.seed(seed)
 
-    if not os.path.exists(GENERATED_DATASET_DIR):
-        os.makedirs(GENERATED_DATASET_DIR)
 
-    DATASET_IMAGES_DIR = GENERATED_DATASET_DIR + '/images/'
+
+
+
+
+
+
+
+
+def create_dataset_dir(OUTPUT_DIR):
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    DATASET_IMAGES_DIR = OUTPUT_DIR + '/images/'
     if not os.path.exists(DATASET_IMAGES_DIR):
         os.makedirs(DATASET_IMAGES_DIR)
     
-    DATASET_LABELS_DIR = GENERATED_DATASET_DIR + '/labels/'
+    DATASET_LABELS_DIR = OUTPUT_DIR + '/labels/'
     if not os.path.exists(DATASET_LABELS_DIR):
         os.makedirs(DATASET_LABELS_DIR)
 
-    classes_dict = generate_yaml_file(PLAYING_CARDS_DIR, GENERATED_DATASET_DIR)
+
+def generate_yaml_file(PLAYING_CARDS_DIR, OUTPUT_DIR):
+    card_names = [frame_name.split('_')[0] for frame_name in os.listdir(PLAYING_CARDS_DIR)]
+    unique_card_names = list(set(card_names))
+    unique_card_names.sort()
+    classes_dict = dict((name, i) for i, name in enumerate(unique_card_names))
+    number_of_classes = len(unique_card_names)
+
+    with open(OUTPUT_DIR + '/classes.yaml', 'w') as file:
+        file.write('classes:\n')
+        for card_name in unique_card_names:
+            file.write('- ' + card_name + '\n')
+        file.write(f'nc: {number_of_classes}')
+    return classes_dict
+
+
+def select_background(BACKGROUNDS_DIR): #todo: resolution 640x640
+    background_paths = Path(BACKGROUNDS_DIR).glob('**/*') # .jpg?
+    selected_background_path = str(rand.sample(background_paths, 1)[0])
+    background = cv.imread(selected_background_path)
+    return background
+
+
+def select_cards(PLAYING_CARDS_DIR, number_of_cards):
+    card_paths = Path(PLAYING_CARDS_DIR).glob('*')
+    selected_cards_paths = [str(card_path for card_path in rand.sample(card_paths, number_of_cards))]
+    selected_cards = [cv.imread(path) for path in selected_cards_paths]
+    selected_cards_names = [card_path.split('/')[-1].split('_')[0] for card_path in selected_cards_paths]
+    return selected_cards, selected_cards_names
+
+
+def transform_card(image, card, size, rotation, angle):
+    image_height, _, _ = image.shape
+    ratio = card.shape[1]/card.shape[0]
+    card_height = int(size*image_height)
+    card_width = int(card_height*ratio)
+
+    card = cv.resize(card, (card_width, card_height))
+    card = imutils.rotate_bound(card, rotation)
+    #todo: angle
+    return card
+
+
+def place_cards(background, cards, card_classes, max_size, min_size, max_rotation, max_angle, overlapping):
+    image = background
+    bounding_boxes = list()
+
+    for card in cards:
+        size = rand.uniform(min_size, max_size)
+        rotation = rand.uniform(0, max_rotation)
+        angle = rand.uniform(0, max_angle) #todo
+        transformed_card = transform_card(image, card, size, rotation, angle)
+        image, bounding_boxes = place_card(image, transformed_card, overlapping, bounding_boxes)
+
+    labels = ''
+    for i, bounding_box in enumerate(bounding_boxes):
+        labels += card_classes[i] + ' ' + ' '.join(bounding_box) + '\n'
+    return image, labels
+
+
+def get_min_max_borders(bounding_box):
+    bb_x_min = bounding_box[0] + bounding_box[2]/2
+    bb_x_max = bounding_box[0] - bounding_box[2]/2
+    bb_y_min = bounding_box[1] + bounding_box[3]/2
+    bb_y_max = bounding_box[1] - bounding_box[3]/2
+    return bb_x_min, bb_x_max, bb_y_min, bb_y_max
+
+
+def no_bounding_boxes_overlap(bounding_boxes, proposed_bounding_box):
+    bb_overlap = False
+    pbb_x_min, pbb_x_max, pbb_y_min, pbb_y_max = get_min_max_borders(proposed_bounding_box)
+
+    for bounding_box in bounding_boxes:
+        bb_x_min, bb_x_max, bb_y_min, bb_y_max = get_min_max_borders(bounding_box)
+        if ((bb_x_min < pbb_x_max) or (pbb_x_min < bb_x_max)) and ((bb_y_min < pbb_y_max) or (pbb_y_min < bb_y_max)):
+            bb_overlap = True
+            break
+
+    return bb_overlap
+
+
+def overlay_images(image, card, x_pos, y_pos):
+    return image #todo
+
+
+def place_card(image, card, overlapping, bounding_boxes):
+    card_placed = False
+    max_tries = 10 #?
+    image_height, image_width, _ = image.shape
+    card_height, card_width, _ = card.shape
+    relative_card_width = card_width/image_width
+    relative_card_heigth = card_height/image_height
+
+    while (not card_placed) and (tries < max_tries):
+        x_pos = rand.uniform(relative_card_width/2, 1 - relative_card_width/2) # todo: half card in picture
+        y_pos = rand.uniform(relative_card_heigth/2, 1 - relative_card_heigth/2)
+        proposed_bounding_box = [x_pos, y_pos, relative_card_width, relative_card_heigth]
+
+        if ((no_bounding_boxes_overlap(bounding_boxes, proposed_bounding_box)) or overlapping): #todo: adjust bbs for overlaps
+            bounding_boxes.append(proposed_bounding_box)
+            image = overlay_images(image, card, x_pos, y_pos)
+            card_placed = True
+        else:
+            tries += 1
+
+    # print(bounding_box)
+    # print(card.shape)
+    # print(image.shape)
+
+    # cv.imshow('image', image)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
+    
+    return image, bounding_box
+
+
+
+
+
+def generate_dataset(BACKGROUNDS_DIR, PLAYING_CARDS_DIR, OUTPUT_DIR, number_of_images, max_number_of_cards_per_image,
+                     min_size, max_size, max_rotation, max_angle, overlapping, seed):
+    rand.seed(seed)
+    create_dataset_dir(OUTPUT_DIR)
+    classes_dict = generate_yaml_file(PLAYING_CARDS_DIR, OUTPUT_DIR)
 
     for i in range(number_of_images):
         background = select_background(BACKGROUNDS_DIR)
-        number_of_cards = rand.randint(1, max_number_of_cards)
-        cards, names = select_cards(PROCESSED_PLAYING_CARDS_DIR, number_of_cards)
-        classes = [classes_dict[name] for name in names]
-        # image, labels = place_cards(background, cards, classes, max_size, min_size, max_rotation, max_angle, overlapping)
+        number_of_cards_per_image = rand.randint(1, max_number_of_cards_per_image)
+        cards, names = select_cards(PLAYING_CARDS_DIR, number_of_cards_per_image)
+        card_classes = [classes_dict[name] for name in names]
+        image, labels = place_cards(background, cards, card_classes, max_size, min_size, max_rotation, max_angle, overlapping)
 
-        # cv.imwrite(GENERATED_DATASET_DIR + '/images/' + i + '.jpeg', image)
-        # with open(GENERATED_DATASET_DIR + '/labels/' + i + '.txt', 'w') as file:
-        #     file.write(labels)
+        cv.imwrite(OUTPUT_DIR + '/images/' + i + '.jpg', image)
+        with open(OUTPUT_DIR + '/labels/' + i + '.txt', 'w') as file:
+            file.write(labels)
 
-    return GENERATED_DATASET_DIR
+    print('Dataset generated and saved at: ' + OUTPUT_DIR)
+    return OUTPUT_DIR
 
 
 
 if __name__ == '__main__':
-    relative_path_videos = '/data/sg_cards'
-    relative_path_cards = '/data/sg_cards_frames_processed'
-    relative_path_backgrounds = '/data/dtd'
-    relative_path_dataset = '/data/sg_cards_dataset_1'
-    seed = 1
+    #input parameters
+    BACKGROUNDS_DIR = '/home/moiki/Documents/Files/studies/4_Semester/ADL/ADL_W23/data/dtd'
+    PLAYING_CARDS_DIR = '/home/moiki/Documents/Files/studies/4_Semester/ADL/ADL_W23/data/sg_cards_processed'
+    OUTPUT_DIR = '/home/moiki/Documents/Files/studies/4_Semester/ADL/ADL_W23/data/sg_cards_dataset'
+
     number_of_images = 2
-    max_number_of_cards = 3
-    overlapping = True
-    max_size = 0.4
+    max_number_of_cards_per_image = 4
     min_size = 0.1
+    max_size = 0.4
     max_rotation = 90
     max_angle = 45
-    #color transformations,...?
-
-    PROJECT_DIR = Path(__file__).parent.parent
-    BACKGROUNDS_DIR = str(PROJECT_DIR) + relative_path_backgrounds
-    PLAYING_CARDS_DIR = str(PROJECT_DIR) + relative_path_cards
-    PROCESSED_PLAYING_CARDS_DIR = str(PROJECT_DIR) + relative_path_cards
-    GENERATED_DATASET_DIR = str(PROJECT_DIR) + relative_path_dataset
+    overlapping = True
+    seed = 8
     
-    generate_dataset(BACKGROUNDS_DIR, PLAYING_CARDS_DIR, PROCESSED_PLAYING_CARDS_DIR, GENERATED_DATASET_DIR, number_of_images, max_number_of_cards,
+    # generate dataset
+    generate_dataset(BACKGROUNDS_DIR, PLAYING_CARDS_DIR, OUTPUT_DIR, number_of_images, max_number_of_cards_per_image,
                      min_size, max_size, max_rotation, max_angle, overlapping, seed)
 
 
